c89d60279e31b5ff19e9a6ee8e00aaa89d458803
sakshijain2409 <98174979+sakshijain2409@users.noreply.github.com>
2024-03-18 03:34:26 +0530

    cnn.py

--- cnn.py
+++ cnn.py
@@ -1,5 +1,1 @@
-google.colab import drive
-import pandas as pd
-drive.mount('/content/drive')
-file_path = "/contentfro/drive/Shared drives/Your Drive Name/dataset.csv"
-df = pd.read_csv(file_path)
+
